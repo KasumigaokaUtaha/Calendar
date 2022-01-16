0e30bc5fc55e07c55cdb35a96527039b4c3f3291
ccjake <37229189+ccjake@users.noreply.github.com>
2022-01-16 07:44:45 +0100

    add "day" button to home

--- RWCalendar/RWCalendar/View/ContentView.swift
+++ RWCalendar/RWCalendar/View/ContentView.swift
@@ -11,46 +11,44 @@ struct ContentView: View {
     @EnvironmentObject var store: AppStore<AppState, AppAction, AppEnvironment>
 
     var body: some View {
-//        NavigationView {
-//            GeometryReader { proxy in
-//                CompactCalendarYearView(size: proxy.size, columnsNumber: 3)
-//            }
-//            .toolbar {
-//                ToolbarItem(
-//                    placement: .navigationBarTrailing
-//                ) {
-//                    Button("Today") {
-//                        store.send(.setScrollToToday(withAnimation: true))
-//                    }
-//                }
-//                ToolbarItem(
-//                    placement: .navigationBarLeading
-//                ) {
-//                    Button {
-//                        // Show context menu
-//                    } label: {
-//                        Image(systemName: "line.3.horizontal")
-//                    }
-//                }
-//
-//                ToolbarItem(
-//                    placement: .navigation
-//                ) {
-//                    NavigationLink("weekday", destination: WeekDays())
-//                }
-//
-//
-//
-//            }
-//            .navigationBarTitleDisplayMode(.inline)
-//            .navigationTitle("Calendar")
-        CalendarDayView()
-            
-            
-//        }
+        NavigationView {
+            GeometryReader { proxy in
+                CompactCalendarYearView(size: proxy.size, columnsNumber: 3)
+            }
+            .toolbar {
+                ToolbarItem(
+                    placement: .navigationBarTrailing
+                ) {
+                    Button("Today") {
+                        store.send(.setScrollToToday(withAnimation: true))
+                    }
+                }
+                ToolbarItem(
+                    placement: .navigationBarLeading
+                ) {
+                    Button {
+                        // Show context menu
+                    } label: {
+                        Image(systemName: "line.3.horizontal")
+                    }
+                }
+
+                ToolbarItem(
+                    placement: .navigationBarLeading
+                ) {
+                    NavigationLink(
+                        destination: CalendarDayView()
+                    ) {
+                        Text("Day")
+                    }
+                }
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationTitle("Calendar")
+        }
         // LayoutConstraints error from NavigationView with navigationTitle
         // https://developer.apple.com/forums/thread/673113?answerId=687012022#687012022
-//        .navigationViewStyle(.stack)
+        .navigationViewStyle(.stack)
     }
 }
 
