c8606298ae2caa0cc655c1048c8ee06f16e26414
KasumigaokaUtaha <blacklotussaki@gmail.com>
2022-01-19 15:33:04 +0100

    fix: App crashes when preview the ContentView

--- RWCalendar/RWCalendar/View/ContentView.swift
+++ RWCalendar/RWCalendar/View/ContentView.swift
@@ -21,28 +21,28 @@ struct ContentView: View {
             }
         case .month:
             ContainerView {
-            // TODO: replace with actual view
+                // TODO: replace with actual view
                 Text("Month")
                     .navigationTitle(String(format: "%d %d", store.state.selectedYear, store.state.selectedMonth))
             }
         case .week:
             ContainerView {
-            // TODO: replace with actual view
+                // TODO: replace with actual view
                 Text("Week")
             }
         case .day:
             ContainerView {
-            // TODO: replace with actual view
+                // TODO: replace with actual view
                 Text("day")
             }
         case .settings:
             ContainerView {
-            // TODO: replace with actual view
+                // TODO: replace with actual view
                 Text("settings")
             }
         case .onboarding:
             ContainerView {
-            // TODO: replace with actual view
+                // TODO: replace with actual view
                 Text("onboarding")
             }
         }
@@ -50,7 +50,22 @@ struct ContentView: View {
 }
 
 struct ContentView_Previews: PreviewProvider {
+    static let store: AppStore<AppState, AppAction, AppEnvironment> = AppStore(
+        initialState: AppState(),
+        reducer: appReducer,
+        environment: AppEnvironment()
+    )
+
     static var previews: some View {
         ContentView()
+            .environmentObject(store)
+            .onAppear {
+                let rangeStart = store.state.currentYear - 1970
+                store.send(.setScrollToToday(withAnimation: false))
+                store.send(.loadYearDataRange(
+                    base: store.state.currentYear,
+                    range: -rangeStart ... 3
+                ))
+            }
     }
 }
